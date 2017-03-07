a188f6c27a03518749d12be20ced9714e05a4014
csjames <jadams8266@gmail.com>
2017-03-07 11:31:24 +0000

    more suited to including

---
+++ shared/packet.cpp
@@ -0,0 +1,11 @@
+#include "packet.h"
+
+void unmarshal_packet (rfpacket *p, uint8_t msg[61]){
+	p->packet_type = msg[0];
+}
+
+void marshal_packet (uint8_t msg[61], rfpacket_t *p){
+	msg[0] = p->packet_type;
+
+	memcpy(msg[PACKET_SIZE-MAX_PAYLOAD],p->data[0], MAX_PAYLOAD);
+}
---
+++ shared/packet.h
@@ -0,0 +1,18 @@
+
+#define MAX_PAYLOAD 		60
+#define PACKET_SIZE			61
+
+#define ACK_PKT 			'A'
+#define BCAST_PKT 			'B'
+#define UNICAST_PKT 		'U'
+
+
+typedef struct {
+	uint8_t packet_type
+	uint8_t data[MAX_PAYLOAD]
+} rfpacket_t;
+
+
+void unmarshal_packet (rfpacket *p, uint8_t msg[61]);
+
+void marshal_packet (uint8_t msg[61], rfpacket_t *p);
