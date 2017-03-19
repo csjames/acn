252728fe410a45e3b7ed395bb88c56c22ef35de3
Sidharth Agarwal <syd@live.in>
2017-03-19 19:44:22 +0000

    forgot some shit lol

--- shared/packet.cpp
+++ shared/packet.cpp
@@ -1,12 +1,85 @@
 #include "packet.h"
 #include <string.h>
+#include <stdlib.h>
 
-void unmarshal_packet (rfpacket_t *p, uint8_t msg[61]){
+rfheader_t history[HISTORY_SIZE];
+int hIndex = 0;
+
+//void push(rfpacket_t *packet);
+//rfpacket_t dequeue();
+
+bool unmarshal_packet (rfpacket_t *p, uint8_t msg[PACKET_SIZE]){
 	p->packet_type = msg[0];
+    p->node_type = msg[1];
+    
+    p->uid = (msg[2] << 8) | msg[3];
+    
+    memcpy(&p->data[0], &msg[PACKET_SIZE-MAX_PAYLOAD], MAX_PAYLOAD);
+    
+    return duplicate_packet(p);
 }
 
-void marshal_packet (uint8_t msg[61], rfpacket_t *p){
+void marshal_packet (uint8_t msg[PACKET_SIZE], rfpacket_t *p){
 	msg[0] = p->packet_type;
-
+    msg[1] = p->node_type;
+    
+    p->uid = rand()%65536;
+    msg[2] = (p->uid >> 8);
+    msg[3] = p->uid;
+    
+    if(!duplicate_packet(p)) {
+        history[hIndex] = *p;
+        hIndex = (++hIndex) % HISTORY_SIZE;
+    }
+    
 	memcpy(&msg[PACKET_SIZE-MAX_PAYLOAD],&p->data[0], MAX_PAYLOAD);
+}
+
+bool duplicate_packet(rfheader_t *inc) {
+    for(int i = 0; i<HISTORY_SIZE; i++) {
+        if(history[i].uid == inc->uid &&
+           history[i].packet_type == inc->packet_type &&
+           history[i].destination == inc->destination)
+            return true;
+    }
+    return false;
+}
+
+void initPacket(unsigned long y) {
+    srand(y);
+}
+
+
+
+struct NODE {
+    rfpacket_t *data;
+    NODE *next;
+};
+
+int queueSize;
+NODE *head;
+NODE *tail;
+
+void push(rfpacket_t *packet) {
+    NODE *newNode = (NODE*) malloc(sizeof (NODE));
+    if(queueSize==0) {
+        head = newNode;
+        tail = newNode;
+    }
+    else {
+        tail->next = newNode;
+    }
+    queueSize++;
+}
+
+rfpacket_t* dequeue() {
+    if(queueSize>0) {
+        rfpacket_t *data = head->data;
+        head = head->next;
+        queueSize--;
+        free (head);
+        return data;
+    }
+    else
+        return 0;
 }
--- shared/packet.h
+++ shared/packet.h
@@ -1,19 +1,34 @@
 #include <stdint.h>
 
-#define MAX_PAYLOAD 		60
+#define MAX_PAYLOAD 		57
 #define PACKET_SIZE			61
+#define HISTORY_SIZE        16
 
 #define ACK_PKT 			'A'
 #define BCAST_PKT 			'B'
 #define UNICAST_PKT 		'U'
 
+#define LP_NODE             'L'
+#define RPT_NODE            'R'
+#define GTW_NODE            'G'
 
 typedef struct {
-	uint8_t packet_type;
+    uint8_t source;
+    uint8_t destination;
+    uint16_t uid;
+    char packet_type;
+    char node_type;
+} rfheader_t;
+
+typedef struct rfpacket_t : public rfheader_t {
+    //rfheader_t header;
 	uint8_t data[MAX_PAYLOAD];
-} rfpacket_t;
+};
 
-
-void unmarshal_packet (rfpacket_t *p, uint8_t msg[61]);
+bool unmarshal_packet (rfpacket_t *p, uint8_t msg[61]);
 
 void marshal_packet (uint8_t msg[61], rfpacket_t *p);
+
+void initPacket(unsigned long y);
+
+bool duplicate_packet(rfheader_t *inc);
